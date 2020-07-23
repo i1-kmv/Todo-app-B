f019447c62357a669916b0582e56f8508b9da9f1
i1-kmv <i1-kmv@mail.ru>
2020-07-23 10:58:05 +0200

    Сформировал компоненты

---
+++ src/components/todo-list-item.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoListItem = () => {
+    return (
+       <span>Drink Coffee</span>
+    )
+}
+
+export default TodoListItem;
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+import React from "react";
+import ReactDOM from 'react-dom';
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
+
+
+
+
+
+const App = () => {
+    return (
+        <>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </>
+    )
+}
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
